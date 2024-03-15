d0e69a23ab04869c167fb5a8f58335da8cac2397
mayankpcp7 <jariwalmayank123@gmail.com>
2024-03-15 14:00:05 +0530

    roots making

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: "Inter",
+        barlow: "Barlow",
+      },
+      colors: {
+        extralightred: "#fa6458",
+        lightred: "#fc3c60",
+        yellow: "#ffbe4a",
+        primaryblack: "#4d4d4d",
+        grey: "#707070",
+        lightwhite: "#b9bdbe",
+        brown: "#4d396",
+        secondaryblack: "#4d4c4a",
+        offwhite: "#c7c4c2",
+      },
+      fontSize: {
+        "6xl": "64px",
+        "2xl": "26px",
+      },
+    },
   },
   plugins: [],
 };
