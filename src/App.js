951c9ced5993edc629dabdb9727178ac6c9e7ab8
David-Saini <lakshaysaini92558@gmail.com>
2024-03-15 16:53:49 +0530

    footer complete faq in progress

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Faqs from './components/Faqs';
 import Footer from './components/Footer';
 import Nav from './components/Nav';
 function App() {
   return (
     <>
       <Nav />
+      <Faqs />
       <Footer />
     </>
   );
---
+++ src/components/common/Helper.jsx
@@ -0,0 +1,33 @@
+export const FaqContent = [
+    {
+        id: "1",
+        title: '1. Why should I use Day to Dusk editing for my listing?',
+        content: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using.',
+    },
+    {
+        id: "2",
+        title: '2. How realistic does the Day to Dusk editing look?',
+        content: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using.',
+    },
+    {
+        id: "3",
+        title: '3. How realistic does the Day to Dusk editing look?',
+        content: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using.',
+    },
+    {
+        id: "4",
+        title: '4. How realistic does the Day to Dusk editing look?',
+        content: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using.',
+    },
+    {
+        id: "5",
+        title: '5. How realistic does the Day to Dusk editing look?',
+        content: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using.',
+    },
+    {
+        id: "6",
+        title: '6. How realistic does the Day to Dusk editing look?',
+        content: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using.',
+    },
+
+];  
