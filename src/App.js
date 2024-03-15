b9d76f0f9b0324232ac1db9819b4aecf253d3ed4
David-Saini <lakshaysaini92558@gmail.com>
2024-03-15 14:03:28 +0530

    comman

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Footer from './components/Footer';
 import Nav from './components/Nav';
 function App() {
   return (
     <>
-    <Nav/>
+      <Nav />
+      <Footer />
     </>
   );
 }
