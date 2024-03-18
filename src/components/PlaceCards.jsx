3442378b7f951014d7d565fe5ac8cb1350608e93
David-Saini <lakshaysaini92558@gmail.com>
2024-03-18 12:17:49 +0530

    Place cards

--- src/components/PlaceCards.jsx
+++ src/components/PlaceCards.jsx
@@ -1,46 +1,51 @@
-import React from 'react'
-import MaldivesIsland from "../assets/images/webp/MaldivesIsland.webp"
-import KarbiIsland from "../assets/images/webp/KarbiBeachThailand.webp"
-import CliffMountainThailand from "../assets/images/webp/CliffMountainThailand.webp"
+import React, { useState } from 'react';
+import { DestinatgionCards } from './common/Helper';
 
 const PlaceCards = () => {
+    const [selectedCard, setSelectedCard] = useState(0); // Defaulting the first card to be active
+    const [showAdditionalContent, setShowAdditionalContent] = useState(true); // Show additional content by default
+
+    const handleCardClick = (index) => {
+        if (selectedCard === index) {
+            setSelectedCard(null);
+            setShowAdditionalContent(false); // Hide additional content when card is unselected
+        } else {
+            setSelectedCard(index);
+            setShowAdditionalContent(true); // Show additional content when card is selected
+        }
+    };
+
     return (
         <div className='max-w-[1164px] mx-auto px-4 md:px-3'>
             <div className='gap-6 flex items-center justify-center flex-wrap -mt-28 md:pb-[30px]'>
+                {DestinatgionCards.map((object, index) => (
+                    <div key={index}>
+                        <div
+                            className={`bg-white rounded-[12px] shadow-CommonShadow ${selectedCard === index ? 'min-h-[535px] max-w-[558px] p-5' : 'max-w-[267px] p-4'}`}
+                            onClick={() => handleCardClick(index)}
+                        >
+                            <img className={`w-full ${selectedCard === index ? 'max-w-[518px] h-[265px]' : 'max-w-[235px] h-[169px]'}`} src={object.DestinationImages} alt="DestinationImages" />
+                            <h3 className={`font-inter text-black !leading-[121%] ${selectedCard === index ? 'text-2xl font-medium mt-5' : 'text-base font-semibold mt-4'}`}>{object.Heading}</h3>
 
-                <div className='max-w-[558px] bg-white rounded-[20px] shadow-CommonShadow p-5'>
-                    <img className='max-w-[518px] w-full' src={MaldivesIsland} alt="MaldivesIsland" />
-                    <h3 className='font-inter font-medium text-2xl text-black mt-5 !leading-[121%]'>Maldives-Island</h3>
-                    <p className='font-inter font-normal opacity-70 text-base text-black mt-3 !leading-[150%]'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using.</p>
-                    <button class="mt-6 rounded-[10px] flex items-center font-semibold font-inter h-[47px] min-w-[131px] overflow-hidden relative group cursor-pointer border text-base border-extralightred bg-transparent text-extralightred">
-                        <span class="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-extralightred top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"></span>
-                        <span class="relative mx-auto text-extralightred text-center whitespace-nowrap transition duration-300 group-hover:text-white ease text-[14px] md:text-base">Read More</span>
-                    </button>
-                </div>
+                            <p className={`font-inter font-normal opacity-70 text-base text-black !leading-[150%] ${selectedCard === index ? 'mt-3' : 'mt-2'}`}>{object.Paragraph}</p>
 
-                <div className='max-w-[267px] bg-white rounded-[12px] shadow-CommonShadow p-4'>
-                    <img className='max-w-[235px] w-full' src={KarbiIsland} alt="KarbiIsland" />
-                    <h3 className='font-inter font-medium text-base text-black mt-4 !leading-[121%]'>long-boat-blue-water</h3>
-                    <p className='font-inter font-normal opacity-70 text-base text-black mt-2 !leading-[150%]'>It is a long established fact that a reader will distracted by</p>
-                    <button class="mt-6 rounded-[10px] flex items-center font-semibold font-inter h-[47px] min-w-[131px] overflow-hidden relative group cursor-pointer border text-base border-extralightred bg-transparent text-extralightred">
-                        <span class="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-extralightred top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"></span>
-                        <span class="relative mx-auto text-extralightred text-center whitespace-nowrap transition duration-300 group-hover:text-white ease text-[14px] md:text-base">Read More</span>
-                    </button>
-                </div>
+                            {selectedCard === index && showAdditionalContent && (
+                                <p className='font-inter font-normal opacity-70 text-base text-black !leading-[150%]'>{object.HideParagraph}</p>
+                            )}
 
-                <div className='max-w-[267px] bg-white rounded-[12px] shadow-CommonShadow p-4'>
-                    <img className='max-w-[235px] w-full' src={CliffMountainThailand} alt="CliffMountainThailand" />
-                    <h3 className='font-inter font-medium text-base text-black mt-4 !leading-[121%]'>long-tail-boat-turquoise</h3>
-                    <p className='font-inter font-normal opacity-70 text-base text-black mt-2 !leading-[150%]'>It is a long established fact that a reader will distracted by</p>
-                    <button class="mt-6 rounded-[10px] flex items-center font-semibold font-inter h-[47px] min-w-[131px] overflow-hidden relative group cursor-pointer border text-base border-extralightred bg-transparent text-extralightred">
-                        <span class="absolute w-64 h-0 transition-all duration-300 origin-center rotate-45 -translate-x-20 bg-extralightred top-1/2 group-hover:h-64 group-hover:-translate-y-32 ease"></span>
-                        <span class="relative mx-auto text-extralightred text-center whitespace-nowrap transition duration-300 group-hover:text-white ease text-[14px] md:text-base">Read More</span>
-                    </button>
-                </div>
+                            <button
+                                className="mt-6 rounded-[10px] flex items-center font-semibold font-inter h-[47px] min-w-[131px] overflow-hidden relative group cursor-pointer border text-base border-extralightred bg-transparent text-extralightred"
+                                onClick={() => handleCardClick(index)}
+                            >
+                                <span className="relative mx-auto text-extralightred text-center whitespace-nowrap transition duration-300 group-hover:text-white ease text-[14px] md:text-base">{object.BtnText}</span>
+                            </button>
 
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default PlaceCards
+export default PlaceCards;
--- src/components/common/Helper.jsx
+++ src/components/common/Helper.jsx
@@ -1,4 +1,7 @@
 import Invert from "../../assets/images/webp/InvertedComa.webp"
+import MaldivesIsland from "../../assets/images/webp/MaldivesIsland.webp"
+import KarbiIsland from "../../assets/images/webp/KarbiBeachThailand.webp"
+import CliffMountainThailand from "../../assets/images/webp/CliffMountainThailand.webp"
 
 export const FaqContent = [
     {
@@ -56,6 +59,34 @@ export const ClientContent = [
         Name: "Rick Wright",
         Text: "Customer",
         Paragraph: "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words.",
+    },
+
+]
+
+export const DestinatgionCards = [
+    {
+        id: 1,
+        DestinationImages: MaldivesIsland,
+        Heading: "Maldives-Island",
+        Paragraph: "It is a long established fact that a reader will distracted by",
+        HideParagraph: "the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using.",
+        BtnText: "Read More",
+    },
+    {
+        id: 2,
+        DestinationImages: KarbiIsland,
+        Heading: "Long-Boat-Blue-Water",
+        Paragraph: "It is a long established fact that a reader will distracted by",
+        HideParagraph: "the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using.",
+        BtnText: "Read More",
+    },
+    {
+        id: 3,
+        DestinationImages: CliffMountainThailand,
+        Heading: "Long-Tail-Boat-Turquoise",
+        Paragraph: "It is a long established fact that a reader will distracted by",
+        HideParagraph: "the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using.",
+        BtnText: "Read More",
     },
 
 ]
